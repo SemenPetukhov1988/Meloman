ac868de36161b2ab9aeb760a97da2e05f25a3bc2
SemenPetukhov <infiav@mail.ru>
2025-11-09 22:29:10 +0300

    Изменил как после проверки преподавателем

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -7,29 +7,30 @@ fun main() {
     // УСЛОВИЕМ ВЫПОЛНЕНИЯ ДОМАШНИХ ЗАДАНИЙ В НЕТОЛОГИИ ЯВЛЯЕТСЯ ИХ ВЫПОЛНЕНИЕ С УЧЕТОМ ПРОЙДЕННОГО МАТЕРИАЛА 
 
 
-    val klientStatus = false // статус клиента как постоянного/не постоянного
-    val prise = 2000 // сумма покупки
+    val klientStatus = true // статус клиента как постоянного/не постоянного
+    val prise = 15000 // сумма покупки
     var priseFinal = 0;
 
-    if (klientStatus == true) {
+    if (klientStatus) {
         if (prise in 0..1000) {
-            priseFinal = prise - (prise * 0.01).toInt()
+            priseFinal = (prise * 0.99).toInt()
         } else if (prise in 1001..10000) {
-            priseFinal = ((prise - 100) * 0.01).toInt()
+            priseFinal = ((prise - 100) * 0.99).toInt()
         } else if (prise > 10001) {
-            priseFinal = prise - ((prise * 0.05) * 0.01).toInt()
+            val intermediatePrice = prise * 0.95
+            priseFinal = (intermediatePrice * 0.99).toInt()
         }
 
 
     }
 
-    if (klientStatus == false) {
+    if (!klientStatus) {
         if (prise in 0..1000) {
             priseFinal = prise
         } else if (prise in 1001..10000) {
             priseFinal = prise - 100.toInt()
         } else if (prise > 10001) {
-            priseFinal = prise - (prise * 0.05).toInt()
+            priseFinal = (prise * 0.95).toInt()
         }
     }
     println("Сумма покупки с учетом скидки составляет $priseFinal")
